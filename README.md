3b96f92c494fff292d8b230ac624fca11f2d750c
SANDIP CHITALE <sandipchitale@gmail.com>
2019-01-11 07:41:47 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# identifierphrase README
+
+An extension to open related file for Angular files.
+
+This adds commands to open related files to left section of status bar.
+
+### 1.0.0
+
+Initial release of identifierphrase
---
+++ src/extension.ts
@@ -0,0 +1,98 @@
+'use strict';
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import * as path from 'path';
+import * as fs from 'fs';
+
+let _statusBarItems = [];
+
+function disposeStatusItems() {
+    _statusBarItems.forEach(_statusBarItem => _statusBarItem.dispose());
+    _statusBarItems = [];
+}
+
+function addStatusItems(editor: vscode.TextEditor) {
+    disposeStatusItems();
+    if (editor) {
+        const fsPath = editor.document.uri.fsPath;
+
+        const fspath = editor.document.uri.fsPath;
+        const basename = path.basename(fsPath);
+        let matches = basename.match(/(.+)(\.spec\.ts)$/);
+        if (matches && matches.length === 3) {
+        } else {
+            matches = basename.match(/(.+)(\.html|\.scss|\.css|\.ts)$/);
+        }
+        if (matches && matches.length === 3) {
+
+            let exts = ['.spec.ts', '.html', '.scss', '.css', '.ts'].filter(ext => ext !== matches[2]);
+
+            const dirname = path.dirname(fsPath);
+            exts.forEach(ext => {
+                if (fs.existsSync(path.join(dirname, matches[1] + ext))) {
+                    const _statusBarItem = vscode.window.createStatusBarItem(
+                        vscode.StatusBarAlignment.Left,
+                        -100000
+                    );
+                    _statusBarItem.tooltip = 'Open ' + ext;
+                    _statusBarItem.text = 'Open ' + ext;
+                    _statusBarItem.color = new vscode.ThemeColor('statusBar.foreground');
+                    _statusBarItem.command = 'extension.open' + ext;
+                    _statusBarItem.show();
+                    _statusBarItems.push(_statusBarItem);
+                }
+            });
+        }
+    }
+}
+
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+    ['.spec.ts', '.html', '.scss', '.css', '.ts'].forEach(ext => {
+        let disposable = vscode.commands.registerCommand(
+            'extension.open' + ext,
+            function () {
+                const editor = vscode.window.activeTextEditor;
+                if (editor) {
+                    const fsPath = editor.document.uri.fsPath;
+                    const basename = path.basename(fsPath);
+                    let matches = basename.match(/(.+)(\.spec\.ts)$/);
+                    if (matches && matches.length === 3) {
+                    } else {
+                        matches = basename.match(/(.+)(\.html|\.scss|\.css|\.ts)$/);
+                    }
+                    if (matches && matches.length === 3) {
+                        const dirname = path.dirname(fsPath);
+                        if (fs.existsSync(path.join(dirname, matches[1] + ext))) {
+                            vscode.workspace
+                                .openTextDocument(path.join(dirname, matches[1] + ext))
+                                .then(doc => vscode.window.showTextDocument(doc));
+                        }
+                    }
+                }
+            }
+        );
+
+        context.subscriptions.push(disposable);
+    })
+
+    const editor = vscode.window.activeTextEditor;
+    if (editor) {
+        addStatusItems(editor);
+    }
+    vscode.window.onDidChangeActiveTextEditor(editor => {
+        if (editor) {
+            addStatusItems(editor);
+        } else {
+            disposeStatusItems();
+        }
+    });
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+    disposeStatusItems();
+}
